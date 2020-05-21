317597cfc50fff05889e5a483ba73d1f23298a80
gaku <pro.gaku@gmail.com>
2020-05-21 15:10:23 +0900

    successとerror実装

--- server/resources/views/examples/index.blade.php
+++ server/resources/views/examples/index.blade.php
@@ -13,25 +13,35 @@
         }
         var ajax = new XMLHttpRequest();
         ajax.responseType = 'json';
-        function http(method, uri, json){
+        function http(method, uri, json, success, error){
             ajax.open(method, uri);
             ajax.setRequestHeader("Content-Type", "application/json");
             ajax.setRequestHeader("X-CSRF-TOKEN", document.getElementsByName('csrf-token')[0].content);
             ajax.send(JSON.stringify(json));
             ajax.onload = () => {
-                console.log(ajax.status);
-                console.log(ajax);
-                console.log("success!");
+                success(ajax);
             };
             ajax.onerror = () => {
-                console.log(ajax.status);
-                console.log("error!");
+                error(ajax)
             };
         }
     </script>
 </head>
 	<body>
 		<h1>こんにちは! Blade!</h1>
-        <button onClick="http(Method.POST,'http://localhost/examples',{'test': 'request'})">click</button>
+        <div>
+            <label for="name">名前</label>
+            <input type="text" name="name" id="name" size="30" maxlength="20" />
+        </div>
+        <button onClick="
+        http(
+            Method.POST,
+            'http://localhost/examples',
+            {
+                'test': document.getElementById('name').value
+            },
+            function(e) { console.log(e); },
+            function(e) { console.log(e); }
+        )">click</button>
 	</body>
 </html>
